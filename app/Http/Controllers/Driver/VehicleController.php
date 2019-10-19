fca8500813488650ff8ec2aae79864f8a8e6ac7e
Victor Yunusa <victoryunusa@Victors-MacBook-Pro.local>
2019-10-19 18:05:51 +0100

    Added addVehicle method to VehicleController

--- app/Http/Controllers/Driver/VehicleController.php
+++ app/Http/Controllers/Driver/VehicleController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Driver;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Driver;
+use App\Vehicle;
+use App\VehicleCategory;
 
 class VehicleController extends Controller
 {
-    public function addVehicle(){
+    public function addVehicle(Request $request){
         
     }
+
 }
