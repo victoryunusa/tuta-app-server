7c7552fdec4ccca03e5b83f08be713887f9396e5
Victor Yunusa <victoryunusa@Victors-MacBook-Pro.local>
2019-10-19 12:53:36 +0100

    Added Driver vehicle controller

---
+++ app/Http/Controllers/Driver/VehicleController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Driver;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class VehicleController extends Controller
+{
+    public function addVehicle(){
+        
+    }
+}
